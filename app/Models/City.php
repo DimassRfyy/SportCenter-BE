82069ef4b7b265d02ef440c8a11b0134db4eb12b
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2024-12-06 16:17:54 +0700

    Add City resource with CRUD functionality and slug generation

--- app/Models/City.php
+++ app/Models/City.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class City extends Model
 {
     protected $fillable = ['name', 'slug', 'icon', 'thumbnail'];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
