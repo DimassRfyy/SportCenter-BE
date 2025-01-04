6d4ca0b106511075180c8a145fe1217da730de95
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2025-01-04 19:03:53 +0700

    Implement index and show methods in CategoryController and CityController for API; add resource responses with place counts and optional limits

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CityApiResource;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $cities = City::withCount('places');
+
+        if ($request->has('limit')) {
+            $cities->limit($request->input('limit'));
+
+            return CityApiResource::collection($cities->get());
+        }
+    }
+
+    public function show(City $city) {
+        $city->load('places');
+        $city->loadCount('places');
+
+        return new CityApiResource($city);
+    }
 }
